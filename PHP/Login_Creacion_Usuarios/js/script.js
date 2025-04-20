471e72bc49a951fbb5a26222625da3af924bc88f
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-04-20 02:35:54 -0600

    Implementacion de funciones de validacion y comportamiento de botones

--- PHP/Login_Creacion_Usuarios/js/script.js
+++ PHP/Login_Creacion_Usuarios/js/script.js
@@ -1,12 +1,13 @@
+// Validacion de la cadena
 function obtenerLlave(tipo) {
-    const llave = prompt("Ingresa la llave (debe ser un valor númerico):");
+    const llave = prompt("Ingresa una llave de máximo 12 carácteres (solo letras a-z sin considerar 'ñ', números del 0 al 9, y carácteres como: '-', '_', '@', '*'):");
+    const regex = /^[a-zA-Z0-9@*_-]{1,12}$/;
     if(llave === null) {
-        alert("No se ingreso ningún valor para la llave.");
+        alert("No se ingresó ningún valor para la llave.");
         return;
     }
-    if(isNaN(llave) || llave.trim()==="") {
-        // Si no es numero o esta vacio
-        alert("Valor incorrecto, se debe ingresar un número.");
+    if(!regex.test(llave)) {
+        alert("La llave no es válida. Ingresa otra.");
         return;
     } else {
         if(tipo==="cifrar") {
@@ -16,5 +17,49 @@ function obtenerLlave(tipo) {
             document.getElementById("llave_descifrado").value = llave;
             document.getElementById("form_descifrar").submit();
         }
+    }
+}
+
+
+// Limpiar cajas de texto
+function limpiarTexto(tipo) {
+    if(tipo==="plano") {
+        document.getElementById("texto_plano").value = "";
+    } else if(tipo==="cifrado") {
+        document.getElementById("texto_cifrado").value = "";
+    }
+}
+
+
+// Copiar el texto de la caja
+function copiarTexto(tipo) {
+    if(tipo==="plano") {
+        // Copiar texto
+        const textArea = document.getElementById("texto_plano");
+        textArea.select();
+        textArea.setSelectionRange(0,99999);
+        document.execCommand("copy");
+
+        // Cambiar icono del boton para indicar que se copio
+        const icono = document.getElementById("icono_copiar_plano");
+        const boton = document.getElementById("boton_copiar_plano");
+        icono.style.display = "inline-block";
+        setTimeout(function() {
+            icono.style.display = "none"; // Ocultar
+        }, 4000); // miliseg
+
+    } else if(tipo==="cifrado") {
+        const textArea = document.getElementById("texto_cifrado");
+        textArea.select();
+        textArea.setSelectionRange(0,99999);
+        document.execCommand("copy");
+
+        // Cambiar icono del boton para indicar que se copio
+        const icono = document.getElementById("icono_copiar_cifrado");
+        const boton = document.getElementById("boton_copiar_cifrado");
+        icono.style.display = "inline-block";
+        setTimeout(function() {
+            icono.style.display = "none"; // Ocultar
+        }, 4000); // miliseg
     }
 }
