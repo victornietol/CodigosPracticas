e4ab18545f99983094e6234069d8b987bb01f86e
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-04-21 01:25:32 -0600

    Actualizacion de la generacion de la llave

--- PHP/Login_Creacion_Usuarios/cifrado_cesar.php
+++ PHP/Login_Creacion_Usuarios/cifrado_cesar.php
@@ -3,15 +3,50 @@ session_start();
 
 // Obtener llave
 function generarLlaveNueva($llave_plana) {
-    $llave_nueva = 0;
+    $sumatoria_parcial = 0;
+    $bloque_bits = '';
+    $bloques_bits = [];
+
     for($i=0; $i<strlen($llave_plana); $i++) {
         $caracter = $llave_plana[$i];
         $c_ascii = ord($caracter); // obtener ascii
         $c_binario = decbin($c_ascii); // obtener binario
-        $c_binario = str_pad($c_binario, 8, "0", STR_PAD_LEFT); // construir los 8 bits por caracter
-        $llave_nueva += bindec($c_binario); // binario a decimal y se va sumando por caracter
+        $c_binario = str_pad($c_binario, 8, "0", STR_PAD_LEFT); // construir los 8 bits por
+        $sumatoria_parcial += bindec($c_binario); // sumar el valor decimal
+        $bloque_bits .= $c_binario; // construyendo el bloque de bits mayor
     }
-    return $llave_nueva;
+
+    // separar en bloques de 4 bits (nibble) el bloque grande
+    for($i=0; $i<strlen($bloque_bits); $i+=4) {
+        $bloques_bits[] = substr($bloque_bits, $i, 4);
+    }
+
+    // intercalar bloques generados
+    $num_bloques = strlen($bloque_bits)/4; // numero de bloques en el que se dividio
+    for($i=0; $i<$num_bloques; $i+=2) {
+        $aux = $bloques_bits[$i];
+        $bloques_bits[$i] = $bloques_bits[$i+1]; 
+        $bloques_bits[$i+1] = $aux;
+    }
+
+    // uniendo los bloques de nuevo
+    $bloque_bits = implode("", $bloques_bits);
+
+    // invirtiendo el valor de los bits
+    $bloque_bits = str_split($bloque_bits); // convertir el string en arreglo
+    for($i=0; $i<count($bloque_bits); $i++) {
+        $bloque_bits[$i] = $bloque_bits=="1" ? "0" : "1";
+    }
+    $bloque_bits = implode("", $bloque_bits); // volviendo a convertir el array a string
+
+    // separando el bloque grande de bits en bloques de 8 (byte) y sumando su valor en decimal a la sumatoria parcial
+    $byte = '';
+    for($i=0; $i<strlen($bloque_bits); $i+=8) {
+        $byte = substr($bloque_bits, $i, 8); // obteniendo segmento
+        $sumatoria_parcial += bindec($byte); // sumando valor decimal
+    }
+
+    return $sumatoria_parcial;
 }
 
 // Aplicar cifrado
