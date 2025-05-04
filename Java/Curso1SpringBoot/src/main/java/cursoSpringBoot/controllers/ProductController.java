814b7804ee9a2cf7a6039197efaea013268426ec
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-04 01:02:52 -0600

    Continuacion curso Spring, Externalizacion de configuraciones

--- Java/Curso1SpringBoot/src/main/java/cursoSpringBoot/controllers/ProductController.java
+++ Java/Curso1SpringBoot/src/main/java/cursoSpringBoot/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package cursoSpringBoot.controllers;
 
+import cursoSpringBoot.configurations.ExternalizedConfigurations;
 import cursoSpringBoot.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,11 +16,17 @@ public class ProductController {
 
     @Autowired
     @Lazy // Indica que la instancia del bean se crea hasta que se utiliza, solo debe utilizarse en los casos requeridos con procesos pesados
-    @Qualifier("jsonResourceService") // Se especifica explicitamente cual bean se quiere inyectar, esta notacion tiene prioridad sobre Primary
+    @Qualifier("jsonResourceService") // Se especifica explicitamente cual bean se quiere inyectar, esta notacion tiene prioridad sobre Primary (Si se utiliza la notacion @ConditionalOnProperty(name="service.products", havingValue="json") en la clase de implementacion, entonces esta notacion no se coloca)
     private ProductService productsService; // Hace referencia a la interface, esto es inyeccion de dependencias. Maneja automaticamente la instancia de ProductsServiceImpl (que es un bean con la notacion '@Service')
+
+    // Inyeccion de las variables de configuracion
+    @Autowired
+    private ExternalizedConfigurations externalizedConfigurations;
 
     @GetMapping
     public ResponseEntity<?> getProducts() {
-       return ResponseEntity.ok(productsService.getProducts());
+        System.out.println(externalizedConfigurations.toString()); // Esta linea no debe ir, solo se hace para mostrar verificar que se inyectan las propiedades de configuracion que se definieron
+        
+        return ResponseEntity.ok(productsService.getProducts());
     }
 }
