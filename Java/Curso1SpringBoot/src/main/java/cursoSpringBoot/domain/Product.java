62b87ef5dcb32924324e9a5caa2e72ee774c735c
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-03 18:02:03 -0600

    Continuacion curso Spring, Implementacion de servicio

---
+++ Java/Curso1SpringBoot/src/main/java/cursoSpringBoot/domain/Product.java
@@ -0,0 +1,58 @@
+package cursoSpringBoot.domain;
+
+public class Product {
+
+    private Integer id;
+    private String nombre;
+    private Double precio;
+    private Integer stock;
+
+    public Product(Integer id, String nombre, Double precio, Integer stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", stock=" + stock +
+                '}';
+    }
+}
