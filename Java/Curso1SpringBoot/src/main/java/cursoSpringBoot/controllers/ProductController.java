acda1765601dfc8aeccdf7e6e58a108c0ea4828c
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-04 00:30:19 -0600

    Continuacion curso Spring, implementacion Inyeccion de dependencias

--- Java/Curso1SpringBoot/src/main/java/cursoSpringBoot/controllers/ProductController.java
+++ Java/Curso1SpringBoot/src/main/java/cursoSpringBoot/controllers/ProductController.java
@@ -1,7 +1,9 @@
 package cursoSpringBoot.controllers;
 
 import cursoSpringBoot.service.ProductService;
-import cursoSpringBoot.service.ProductsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-    ProductService productsService = new ProductsServiceImpl(); // Polimorfismo, se utiliza la interface
+
+    @Autowired
+    @Lazy // Indica que la instancia del bean se crea hasta que se utiliza, solo debe utilizarse en los casos requeridos con procesos pesados
+    @Qualifier("jsonResourceService") // Se especifica explicitamente cual bean se quiere inyectar, esta notacion tiene prioridad sobre Primary
+    private ProductService productsService; // Hace referencia a la interface, esto es inyeccion de dependencias. Maneja automaticamente la instancia de ProductsServiceImpl (que es un bean con la notacion '@Service')
 
     @GetMapping
     public ResponseEntity<?> getProducts() {
