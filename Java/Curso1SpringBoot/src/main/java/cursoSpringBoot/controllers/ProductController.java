cbc55924098c49e88d3adc08108d3bbfa157e0ab
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-03 18:33:31 -0600

    Continuacion de curso Spring, implementacion de Interfaces

--- Java/Curso1SpringBoot/src/main/java/cursoSpringBoot/controllers/ProductController.java
+++ Java/Curso1SpringBoot/src/main/java/cursoSpringBoot/controllers/ProductController.java
@@ -1,17 +1,16 @@
 package cursoSpringBoot.controllers;
 
+import cursoSpringBoot.service.ProductService;
 import cursoSpringBoot.service.ProductsServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-    ProductsServiceImpl productsService = new ProductsServiceImpl();
+    ProductService productsService = new ProductsServiceImpl(); // Polimorfismo, se utiliza la interface
 
     @GetMapping
     public ResponseEntity<?> getProducts() {
