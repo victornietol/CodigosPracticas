44293acee52012e42894a92281c4b3014b0b2123
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-03 18:01:13 -0600

    Continuacion curso Spring, Implementacion de servicio

---
+++ Java/Curso1SpringBoot/src/main/java/cursoSpringBoot/service/ProductsServiceImpl.java
@@ -0,0 +1,22 @@
+package cursoSpringBoot.service;
+
+import cursoSpringBoot.domain.Product;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductsServiceImpl {
+
+    // Los datos de la siguiente lista deben ir en una base de datos, por el momento solo es un ejemplo
+    List<Product> products = new ArrayList<>(Arrays.asList(
+            new Product(0, "TV", 4500.50, 20),
+            new Product(1, "Mouse", 1500.00, 10),
+            new Product(2, "Teclado", 2200.50, 20),
+            new Product(0, "Telefono", 5300.00, 15)
+    ));
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
