20304076ac24338fb4ff2dd7ec3221e2cd747b0a
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-21 15:20:23 -0600

    Configuraciones de los modelos (BD)

--- Python/django-crud-react-ejemplo1/tasks/models.py
+++ Python/django-crud-react-ejemplo1/tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    done = models.BooleanField(default=False)
+
+    def __str__(self): # Indica que texto va a mostrar al imprimir el objeto
+        return self.title
