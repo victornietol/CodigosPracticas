3e35bf6f1756159397f438f3c83d9bb5a820d042
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-22 17:04:57 -0600

    Creacion de tareas desde front

--- Python/django-crud-react-ejemplo1/client/src/api/tasks.api.js
+++ Python/django-crud-react-ejemplo1/client/src/api/tasks.api.js
@@ -1,7 +1,14 @@
-// Se encarga de hacer la peticion a la api
+// Se encarga de hacer peticiones a la api
 
 import axios from 'axios';
 
-export const getAllTasks = () => {
-    return axios.get("http://127.0.0.1:8000/task/api/v1/tasks/");
-}
+// Asignar la ruta para las peticiones
+const tasksApi = axios.create({
+    baseURL: "http://127.0.0.1:8000/task/api/v1/tasks/"
+})
+
+// Obtener datos
+export const getAllTasks = () => tasksApi.get("/");
+
+// Enviar datos
+export const createTask = (task) => tasksApi.post("/", task);
