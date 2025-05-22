56d8d75417a1d92b826c424bd08d76f2723e0058
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-22 17:39:06 -0600

    Funcionamiento para eliminar elementos desde front

--- Python/django-crud-react-ejemplo1/client/src/components/TaskCard.jsx
+++ Python/django-crud-react-ejemplo1/client/src/components/TaskCard.jsx
@@ -1,9 +1,18 @@
 {/* Componente que construye la vista de cada tarea para mostrarla,
     recibe como parametro la tarea como parametro*/}
 
+import { useNavigate } from 'react-router-dom';
+
 export function TaskCard({ task }) {
+
+    const navigate = useNavigate();
+
     return (
-        <div>
+        <div 
+            onClick={() => {
+                navigate(`/tasks/${task.id}`)
+            }}
+        >
             <h1>{task.title}</h1>
             <p>{task.description}</p>
             <hr />
