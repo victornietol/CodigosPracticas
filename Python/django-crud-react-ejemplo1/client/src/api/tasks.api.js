1f4b063a5b059ca0596315c7589b89a53d33a7cd
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-22 18:09:23 -0600

    Funcionamiento de eliminar desde front

--- Python/django-crud-react-ejemplo1/client/src/api/tasks.api.js
+++ Python/django-crud-react-ejemplo1/client/src/api/tasks.api.js
@@ -7,11 +7,17 @@ const tasksApi = axios.create({
     baseURL: "http://127.0.0.1:8000/task/api/v1/tasks/"
 })
 
-// Obtener datos
+// Obtener todos los datos
 export const getAllTasks = () => tasksApi.get("/");
+
+// Obtener un solo elemento
+export const getTask = (id) => tasksApi.get(`/${id}/`);
 
 // Enviar datos
 export const createTask = (task) => tasksApi.post("/", task);
 
 // Eliminar datos (por su id)
 export const deleteTask = (id) => tasksApi.delete(`/${id}`);
+
+// Actualizar datos
+export const updateTask = (id, task) => tasksApi.put(`/${id}/`, task)
