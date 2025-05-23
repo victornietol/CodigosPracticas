53099d30419bec41a45703a018ba3f7e89c1e373
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-22 19:05:19 -0600

    Agregando Tailwind para estilos

--- Python/django-crud-react-ejemplo1/client/src/pages/TaskFormPage.jsx
+++ Python/django-crud-react-ejemplo1/client/src/pages/TaskFormPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { createTask, deleteTask, updateTask, getTask } from '../api/tasks.api';
 import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
 
 export function TasksFormPage() {
 
@@ -18,9 +19,23 @@ export function TasksFormPage() {
         if (params.id) {
             // Actualizando
             await updateTask(params.id, data);
+            toast.success( "Tarea actualizada", {  // Notificacon de cambio
+                position: "bottom-right",
+                style: {
+                    background: "#101010",
+                    color: "#fff"
+                }
+            });
         } else {
             // Creando
             await createTask(data);
+            toast.success( "Tarea creada", {  // Notificacon de cambio
+                position: "bottom-right",
+                style: {
+                    background: "#101010",
+                    color: "#fff"
+                }
+            });
         }
         navigate("/tasks");
     });
@@ -37,12 +52,13 @@ export function TasksFormPage() {
     });
 
     return (
-        <div>
+        <div className='max-w-xl mx-auto'>
             <form onSubmit={onSubmit}>
                 <input 
                     type="text" 
                     placeholder="title" 
                     {...register("title", { required: true })}
+                    className='bg-zinc-700 p-3 rounded-lg block w-full mb-3'
                 />
                 {errors.title && <span>El titulo es requerido</span>}
 
@@ -50,19 +66,38 @@ export function TasksFormPage() {
                     rows="3" 
                     placeholder="description" 
                     {...register("description", { required:true })}
-                />
+                    className='bg-zinc-700 p-3 rounded-lg block w-full mb-3'
+                ></textarea>
                 {errors.description && <span>La descripcion es requerida</span>}
 
-                <button>Guardar</button>
+                <button className='bg-indigo-500 p-3 rounded-lg block w-full mt-3'>
+                    Guardar
+                </button>
             </form>
 
-            {params.id && <button onClick={async () => {
-                const confirm = window.confirm("¿Estas seguro?");
-                if (confirm) {
-                    await deleteTask(params.id);
-                    navigate("/tasks")
-                }
-            }} >Eliminar</button>}
+            {params.id && (
+                <div className='flex justify-end'>
+                    <button 
+                        className='bg-red-500 p-3 rounded-lg w-48 mt-3'
+                        onClick={async () => {
+                            const confirm = window.confirm("¿Estas seguro?");
+                            if (confirm) {
+                                await deleteTask(params.id);
+                                toast.success( "Tarea eliminada", {  // Notificacon de cambio
+                                    position: "bottom-right",
+                                    style: {
+                                        background: "#101010",
+                                        color: "#fff"
+                                    }
+                                });
+                                navigate("/tasks");
+                            }
+                        }} 
+                    >
+                        Eliminar
+                    </button>
+                </div>
+            )}
         </div>
     );
 }
