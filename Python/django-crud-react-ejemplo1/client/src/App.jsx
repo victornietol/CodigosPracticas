2229cd84dad397f8a248d6a9b3e02b7c1f03a672
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-22 19:04:57 -0600

    Agregando Tailwind para estilos

--- Python/django-crud-react-ejemplo1/client/src/App.jsx
+++ Python/django-crud-react-ejemplo1/client/src/App.jsx
@@ -2,18 +2,23 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { TasksPage } from './pages/TasksPage';
 import { TasksFormPage } from './pages/TaskFormPage';
 import { Navigation } from './components/Navigation';
+import { Toaster } from 'react-hot-toast'; // Noticaciones al realizar cambios 
 
 function App() {
   return (
     <BrowserRouter>
-      <Navigation />
+      <div className="container mx-auto">
+        <Navigation />
 
-      <Routes>
-        <Route path="/" element={<Navigate to="/tasks" />} /> {/* Indica a que ruta se redirecciona al visitar el inicio, esto es opcional dependiendo del proyecto */}
-        <Route path="/tasks" element={<TasksPage />} />
-        <Route path="/tasks-create" element={<TasksFormPage />} />
-        <Route path="/tasks/:id" element={<TasksFormPage />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Navigate to="/tasks" />} /> {/* Indica a que ruta se redirecciona al visitar el inicio, esto es opcional dependiendo del proyecto */}
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/tasks-create" element={<TasksFormPage />} />
+          <Route path="/tasks/:id" element={<TasksFormPage />} />
+        </Routes>
+
+        <Toaster />
+      </div>
 
     </BrowserRouter>
   );
