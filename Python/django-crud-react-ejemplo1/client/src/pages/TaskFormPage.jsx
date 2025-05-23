fba9f4b606461551968c33c75896ef1c8c4632f5
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-22 18:08:56 -0600

    Funcionamiento de eliminar desde front

--- Python/django-crud-react-ejemplo1/client/src/pages/TaskFormPage.jsx
+++ Python/django-crud-react-ejemplo1/client/src/pages/TaskFormPage.jsx
@@ -1,16 +1,39 @@
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { createTask, deleteTask } from '../api/tasks.api';
+import { createTask, deleteTask, updateTask, getTask } from '../api/tasks.api';
 import { useNavigate, useParams } from 'react-router-dom';
 
 export function TasksFormPage() {
 
-    const { register, handleSubmit, formState: { errors } } = useForm(); {/* Manejar formulario (registra campos y hacer validaciones) */}
+    const { 
+        register, 
+        handleSubmit, 
+        formState: { errors },
+        setValue // Permiite colocar valores al formulario
+    } = useForm(); {/* Manejar formulario (registra campos y hacer validaciones) */}
     const navigate = useNavigate(); {/* Navegador para indicar que hacer al guardar los datos */}
     const params = useParams();  // Obtener parametros de la url (id en este caso)
 
     const onSubmit = handleSubmit(async data => { // Funcion para guardar los datos introducidos en el formulario
-        await createTask(data);
+        if (params.id) {
+            // Actualizando
+            await updateTask(params.id, data);
+        } else {
+            // Creando
+            await createTask(data);
+        }
         navigate("/tasks");
+    });
+
+    useEffect(() => {
+        async function loadTask() { // Como es una funcion asincrona, no puede ir directamente dentro de useEffect, sino que debe colocarse en otra funcion
+            if(params.id) {
+                const response = await getTask(params.id);
+                setValue("title", response.data.title);
+                setValue("description", response.data.description);
+            }
+        }
+        loadTask();
     });
 
     return (
