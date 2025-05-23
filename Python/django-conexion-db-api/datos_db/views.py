6d8b57867cabf76834e4c1b1d353aece5d0b7add
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-23 17:59:38 -0600

    Se agrego ruta personalizada para buscar una Entidad por nombre

--- Python/django-conexion-db-api/datos_db/views.py
+++ Python/django-conexion-db-api/datos_db/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from .serializer import ProductorSerializer, TransportistaSerializer, EntidadSerializer
 from .models import Productor, Transportista, Entidades
 
@@ -15,3 +17,18 @@ class TransportistaView(viewsets.ReadOnlyModelViewSet):
 class EntidadView(viewsets.ReadOnlyModelViewSet):
     serializer_class = EntidadSerializer
     queryset = Entidades.objects.all()
+
+    # Endpoint personalizado
+    @action(detail=False, methods=['get'], url_path="by_name/(?P<nombre>[^/.]+)")
+    def search_by_name(self, request, nombre=None):
+        element = Entidades.objects.filter(nombre__iexact=nombre)
+        if not element.exists():
+            return Response(
+                {"error": "Elemento no encontrado"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = self.get_serializer(element, many=True)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
