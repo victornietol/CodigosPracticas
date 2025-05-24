7c394753093f0efb651b5b870a9b7df716639041
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-05-23 19:35:38 -0600

    Implementacion de endpoints para registrarse y obtener token para hacer peticiones a un endpoint protegido

--- Python/django-conexion-db-api/pruebaConBD/settings.py
+++ Python/django-conexion-db-api/pruebaConBD/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "datos_db"
+    "rest_framework.authtoken",
+    "datos_db",
     #"datosDB",
 ]
 
