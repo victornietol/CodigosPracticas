146647c2408bb27cbe8ec92d8aca8e0194c83847
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-06-12 16:08:43 -0600

    Ejercicio agenda continuacion

--- Python/djangoEjercicioAgenda/contact/urls.py
+++ Python/djangoEjercicioAgenda/contact/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.index, name="contact"),
     path("view/<int:id>", views.view, name="contact_view"),
     path("edit/<int:id>", views.edit, name="contact_edit"),
+    path("create/", views.create, name="contact_create"),
+    path("delete/<int:id>", views.delete, name="contact_delete"),
 ]
--- Python/djangoEjercicioAgenda/contact/views.py
+++ Python/djangoEjercicioAgenda/contact/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact
 from .forms import ContactForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -18,10 +19,42 @@ def view(request, id):
     return render(request, "contact/detail.html", context)
 
 def edit(request, id):
+    contact = Contact.objects.get(id=id)
+
     if request.method == 'GET':
-        contact = Contact.objects.get(id=id)
         form = ContactForm(instance=contact)
+        context = {
+            "form": form,
+            "id": id
+        }
+        return render(request, 'contact/edit.html', context)
+
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+        context = {
+            "form": form,
+            "id": id
+        }
+        messages.success(request, "Contacto actualizado.")
+        return render(request, 'contact/edit.html', context)
+    
+def create(request):
+    if request.method == "GET":
+        form = ContactForm()
         context = {
             "form": form
         }
-        return render(request, 'contact/create.html', context)
+        return render(request, "contact/create.html", context)
+    
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("contact")
+    
+def delete(request, id):
+    contact = Contact.objects.get(id=id)
+    contact.delete()
+    return redirect("contact")
