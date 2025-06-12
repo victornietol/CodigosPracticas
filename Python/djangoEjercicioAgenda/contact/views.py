1cb8b55fd548a7970ccecbbe0a9e576c0f2a902a
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-06-12 01:22:29 -0600

    Ejercicio agenda continuacion

--- Python/djangoEjercicioAgenda/contact/views.py
+++ Python/djangoEjercicioAgenda/contact/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
+def index(request):
+    contacts = Contact.objects.filter(name__contains=request.GET.get('search', '')) # (valor de busqueda, valor por defecto), el valor por defecto es '' porque con este trae todos los elementos
+    context = {
+        'contacts': contacts
+    }
+    return render(request, 'contact/index.html', context)
