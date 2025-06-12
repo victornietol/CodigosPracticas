ea6c4fb8ef4f37ea993806e3ce45491527b4e447
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-06-12 17:07:30 -0600

    continuacion ejericio agenda

--- Python/djangoEjercicioAgenda/todo/forms.py
+++ Python/djangoEjercicioAgenda/todo/forms.py
@@ -1,7 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput
 from .models import Todo
 
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
         fields = "__all__"
+        widgets = {
+            "estimated_end": DateInput(attrs={"type":"date"}),
+            "date": DateInput(attrs={"type":"date"})
+        }
