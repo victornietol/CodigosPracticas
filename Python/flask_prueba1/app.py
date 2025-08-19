45d423ee7dc1b40f5bcc752925bd5b5e6b4b4601
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-08-18 23:25:01 -0600

    Se agrego contenido html en "templates" y logica para su manejo en app.py

--- Python/flask_prueba1/app.py
+++ Python/flask_prueba1/app.py
@@ -1,12 +1,59 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, session, redirect, url_for
 import operations as op
+from functools import wraps
 
 app = Flask(__name__)
+app.secret_key = "clave_prueba_secreta"
 
-# HTML
+def login_logic(username, password):
+    code_response = op.login(username, password)
+    if code_response == 200:
+        return {"message": "Inicio de sesión exitoso."}, code_response
+    elif code_response == 401:
+        return {"message": "Username o password incorrectos."}, code_response
+    
+# Decoradores para verificar inicio de session
+def login_required(function):
+    @wraps(function)
+    def decorated_function(*args, **kwargs):
+        if "username" not in session: # No hay sesion iniciada
+            return redirect(url_for("login_template"))
+        return function(*args, **kwargs)
+    return decorated_function
+
+
+
+
+# Front
 @app.route("/")
+@login_required
 def index():
-    return render_template("index.html")
+    return render_template("index.html", username=session["username"])
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login_template():
+    if request.method == "GET":
+        render = render_template("login.html")
+
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        response, code_status = login_logic(username, password)
+        if code_status == 200:
+            session["username"] = username # Guardar inicio de sesion
+            render = redirect(url_for("index"))
+        elif code_status == 401:
+            render = render_template("login.html", message=response.get("message"))
+
+    return render
+
+
+@app.route("/register")
+def register_template():
+    return render_template("register.html")
+
+
 
 
 
@@ -19,6 +66,7 @@ def list_users():
     '''
     users = op.list_usernames()
     return jsonify(users), 200
+
 
 @app.get("/api/v1/users")
 def get_users():
@@ -45,12 +93,8 @@ def login():
     username = data.get("username")
     password = data.get("password")
 
-    code_response = op.login(username, password)
-    if code_response==200:
-        message = "Inicio de sesión exitoso."
-    elif code_response==401:
-        message = "Username o password incorrectos."
-    return jsonify({"message": message}), code_response
+    response, code_status = login_logic(username, password)
+    return jsonify(response), code_status
 
 
 @app.post("/api/v1/user/register")
