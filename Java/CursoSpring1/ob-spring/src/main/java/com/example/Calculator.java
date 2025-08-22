0d633952f4566735fa51482a56f6c068310d0b23
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-08-21 22:56:26 -0600

    CursoSpring1

---
+++ Java/CursoSpring1/ob-spring/src/main/java/com/example/Calculator.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Calculator {
+
+    public Calculator() {
+        System.out.println("Prueba de constructor Calculator");
+    }
+
+    public String ejemplo(){
+        return "Prueba de bean calculator";
+    }
+}
