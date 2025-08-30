eeb9926b6cbd22cc706aca8865064d4c31865850
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-08-29 18:52:15 -0600

    Continuacion curso spring, deploy

---
+++ Java/CursoSpring1/spring-deploy/src/main/java/com/example/spring_deploy/TestController.java
@@ -0,0 +1,38 @@
+package com.example.spring_deploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${app.varexample}") // Obtener el valor de una variable establecida en el archivo de configuraciones
+    String message;
+
+    @GetMapping("/test1")
+    public String test1() {
+        System.out.println(message);
+        return "Test1";
+    }
+
+    @GetMapping("/bootstrap")
+    public String bootstrap() {
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Hello, world!</h1>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+                  </body>
+                </html>
+                
+                """;
+    }
+}
---
+++ Java/CursoSpring1/spring-deploy/src/test/java/com/example/spring_deploy/SpringDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
