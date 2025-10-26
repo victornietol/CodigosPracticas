32037cef359bb13eb66511fda2d8b3634094f8bd
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-10-26 00:15:19 -0600

    Se agregaron rutas

--- Python/Prueba_DockerFastAPI/main.py
+++ Python/Prueba_DockerFastAPI/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from routes import mamifero_routes
+from routes import mamifero_routes, ave_router, reptil_router
 
 app = FastAPI(title="Prueba FastAPI")
 
 app.include_router(mamifero_routes.router)
+app.include_router(ave_router.router)
+app.include_router(reptil_router.router)
