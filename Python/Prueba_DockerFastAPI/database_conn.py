d9dd7d55c9fb98fc7b1b59d6123b7730026c6f8d
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-10-27 22:42:46 -0600

    Configuraciones para docker

--- Python/Prueba_DockerFastAPI/database_conn.py
+++ Python/Prueba_DockerFastAPI/database_conn.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:1234@127.0.0.1:3307/animals"
+# Se indica el nombre del contenedor de la BD en lugar de la ip para que se pueda hacer 
+# la conexion a traves de la red creada en docker
+#
+# El contenedor de la BD es mysql_docker_1:3306   , en este caso se debe indicar el 
+# puerto del contenedor docker y no el puerto del host
+DATABASE_URL = "mysql+pymysql://root:1234@mysql_docker_1:3306/animals"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
