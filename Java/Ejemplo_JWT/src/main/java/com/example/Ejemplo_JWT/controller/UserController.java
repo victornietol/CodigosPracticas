ae32721e8b11758f6884d8a840117e2eea6d9343
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-10-28 22:50:39 -0600

    Actualizacion de configuraciones

--- Java/Ejemplo_JWT/src/main/java/com/example/Ejemplo_JWT/controller/UserController.java
+++ Java/Ejemplo_JWT/src/main/java/com/example/Ejemplo_JWT/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.example.Ejemplo_JWT.controller;
 
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Peticiones para verificar credenciales
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class UserController {
 }
--- Java/Ejemplo_JWT/src/main/java/com/example/Ejemplo_JWT/entity/AuthRequest.java
+++ Java/Ejemplo_JWT/src/main/java/com/example/Ejemplo_JWT/entity/AuthRequest.java
@@ -1,4 +1,13 @@
 package com.example.Ejemplo_JWT.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRequest {
+    private String username;
+    private String password;
 }
