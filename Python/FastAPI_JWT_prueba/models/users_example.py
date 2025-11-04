54956fd1c6ed3d176dcd5d32d6e5c1afc10c77bb
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-11-03 20:02:02 -0600

    Datos prueba para autenticacion

--- Python/FastAPI_JWT_prueba/models/users_example.py
+++ Python/FastAPI_JWT_prueba/models/users_example.py
@@ -1,1 +1,6 @@
 # Ejemplo de usuarios existentes declarado de esta manera para el ejemplo.
+
+USERS = [
+    {"id": 1, "username": "admin", "password": "1234"},
+    {"id": 2, "username": "user", "password": "1234"}
+]
