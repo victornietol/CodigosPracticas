7adca516e8c55d9983ecfd7a981ce1363fb28d7b
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-11-06 20:45:54 -0600

    Spring batch. Configuraciones iniciales de ejemplo

---
+++ Java/spring-batch-ejemplo/src/test/java/com/example/spring_batch_ejemplo/SpringBatchEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_batch_ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
