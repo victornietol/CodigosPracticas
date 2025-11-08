5239cfd674bee06beb00932a88fd330711870add
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2025-11-07 22:27:46 -0600

    curso repaso CSharp

---
+++ CSharp/curso_CSharp1/primerEjemplo_ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using Humanizer;
+
+Console.WriteLine("Numero 1: ");
+var text = int.Parse(Console.ReadLine());
+Console.WriteLine("Numero 2: ");
+var text2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"text1 = {text.ToWords()}, text2 = {text2.ToWords()}");
