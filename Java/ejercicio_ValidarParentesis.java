bff5aa951c9ee4001f17f6af22269eb3c7714ae2
Victor Nieto <72120913+victornietol@users.noreply.github.com>
2026-05-12 00:53:33 -0600

    ejercicio para validar parentesis y simbolos

--- Java/ejercicio_ValidarParentesis.java
+++ Java/ejercicio_ValidarParentesis.java
@@ -5,8 +5,37 @@ import java.util.Stack;
 public class ejercicio_ValidarParentesis {
     public static void main(String[] args) {
         String s = "{[]}";
+        System.out.println(isValid(s));
 
-        System.out.println(valid(s));
+        s = "]";
+        System.out.println(isValid(s));
+
+        s = "mensaje (PRUEBA) va{li[d]o}";
+        System.out.println(isValid(s));
+
+        s = "[[{)}]";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String str) {
+        /**
+         * Validar con texto completo, no solo simbolos
+         */
+        Stack<Character> stack = new Stack<>();
+        for (char c: str.toCharArray()) {
+            if (c == '[' || c=='{' || c=='(') stack.push(c);
+            else if (c==']' || c=='}' || c==')') {
+                if (stack.isEmpty())
+                    return false;
+                else if (c == ']' && stack.pop() != '[')
+                    return false;
+                else if (c == '}' && stack.pop() != '{')
+                    return false;
+                else if (c == ')' && stack.pop() != '(')
+                    return false;
+            }
+        }
+        return stack.isEmpty(); // si esta vacio significa que se cerraron todos los simbolos
     }
 
     public static boolean valid(String s){
@@ -14,12 +43,10 @@ public class ejercicio_ValidarParentesis {
 
         for (char c : s.toCharArray()) {
 
-            // Abrir
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             }
 
-            // Cerrar
             else {
                 if (stack.isEmpty()) {
                     return false;
